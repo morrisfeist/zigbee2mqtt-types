38efaad21b17578d1e3621e6c7dc47912cea3d5a
Tom Morris Feist <morris.feist@googlemail.com>
2024-09-27 01:28:09 +0200

    Add generated hs source as nix package

--- flake.nix
+++ flake.nix
@@ -13,5 +13,28 @@
     in
     {
       devShells = forAllSystems (pkgs: { default = import ./shell.nix { inherit pkgs; }; });
+
+      packages = forAllSystems (pkgs: rec {
+        default = zigbee2mqtt-types-source;
+        zigbee2mqtt-types-source =
+          let
+            packageYaml = builtins.path { path = ./package.yaml; };
+            hsSrc = builtins.path { path = ./src; };
+          in
+          pkgs.buildNpmPackage {
+            pname = "zigbee2mqtt-types-source";
+            version = "0.0.1";
+            src = ./generator;
+            npmDepsHash = "sha256-B4I8JFMXkk7Se3Kup9kc5z+EBylP5qdCTQlaT62vn7Q=";
+            dontNpmBuild = true;
+            installPhase = ''
+              mkdir -p $out
+              npm start -- $out ${packageYaml}
+
+              mkdir -p $out/src
+              cp -r ${hsSrc}/* $out/src
+            '';
+          };
+      });
     };
 }
--- generator/generate.js
+++ generator/generate.js
@@ -1,10 +1,86 @@
 const dataSource = require("zigbee-herdsman-converters");
 const fs = require("node:fs");
 const path = require("node:path");
+const yaml = require("yaml");
 
-for (const definition of dataSource.definitions) {
-  const model = definition.model.replace(/[^a-zA-Z0-9]/g, "_");
-  const definitionPath = `../definitions/${definition.vendor}/${model}.json`;
-  fs.mkdirSync(path.dirname(definitionPath), { recursive: true });
-  fs.writeFileSync(definitionPath, JSON.stringify(definition, null, 4));
+function parseArguments() {
+  if (process.argv.length <= 3) {
+    console.error("Usage: npm start <output-dir> <package-yaml-path>");
+    process.exit(1);
+  }
+
+  outputPath = process.argv[2];
+  packageYamlPath = process.argv[3];
+
+  if (!fs.existsSync(outputPath) || !fs.lstatSync(outputPath).isDirectory()) {
+    console.error("No such directory " + outputPath);
+    process.exit(1);
+  }
+
+  if (
+    !fs.existsSync(packageYamlPath) ||
+    !fs.lstatSync(packageYamlPath).isFile()
+  ) {
+    console.error("No such file " + packageYamlPath);
+    process.exit(1);
+  }
+
+  return { outputPath, packageYamlPath };
 }
+
+function writeDefinitionFiles({ outputPath }) {
+  const sanitizeStr = (str) =>
+    str
+      .replace(/[^a-zA-Z0-9]+/g, "_")
+      .replace(/^_+/, "")
+      .replace(/_+$/, "");
+
+  for (const definition of dataSource.definitions) {
+    const vendor = sanitizeStr(definition.vendor);
+    const model = `${sanitizeStr(definition.model)}`;
+
+    const definitionPath = `${outputPath}/definitions/${vendor}/${model}.json`;
+    fs.mkdirSync(path.dirname(definitionPath), { recursive: true });
+    fs.writeFileSync(definitionPath, JSON.stringify(definition, null, 4));
+  }
+}
+
+function writeDeviceFiles({ outputPath }) {
+  const sanitizeStr = (str) =>
+    str
+      .replace(/[^a-zA-Z0-9]+/g, "_")
+      .replace(/^_+/, "")
+      .replace(/_+$/, "");
+  const exposedModules = [];
+
+  for (const definition of dataSource.definitions) {
+    const vendor = sanitizeStr(definition.vendor);
+    const model = sanitizeStr(definition.model);
+
+    const definitionPath = `${outputPath}/src/Zigbee2MQTT/Devices/${vendor}_${model}.hs`;
+    const module = `Zigbee2MQTT.Devices.${vendor}_${model}`;
+    exposedModules.splice(0, 0, module);
+    const fileContent = [`module ${module} where`].join("\n");
+    fs.mkdirSync(path.dirname(definitionPath), { recursive: true });
+    fs.writeFileSync(definitionPath, fileContent);
+  }
+
+  return exposedModules;
+}
+
+function writePackageYaml({ outputPath, packageYamlPath }, exposedModules) {
+  const yamlStr = fs.readFileSync(packageYamlPath, { encoding: "utf-8" });
+  const packageYaml = yaml.parse(yamlStr);
+  packageYaml.library["exposed-modules"].splice(0, 0, ...exposedModules);
+  fs.writeFileSync(`${outputPath}/package.yaml`, yaml.stringify(packageYaml));
+}
+
+function main() {
+  config = parseArguments();
+
+  writeDefinitionFiles(config);
+  const exposedModules = writeDeviceFiles(config);
+  writePackageYaml(config, exposedModules);
+}
+
+main();
